9a74c5262ac7dc8017b758c1abf282c902cd1bac
MaXerri <xmario02@yahoo.com>
2025-03-04 13:38:40 -0500

    deployment test2

--- active.js
+++ active.js
@@ -3,10 +3,12 @@
 const navLinksEls = document.querySelectorAll('.nav__link');
 const windowPathname = window.location.pathname;
 
+const isHomePage = windowPathname === '/' || windowPathname === '' || windowPathname.endsWith('index.html');
+
 navLinksEls.forEach(navLinkEl => {
     const navLinkPathname = new URL(navLinkEl.href).pathname;
     
-    if ((windowPathname === navLinkPathname) || ((windowPathname === '/' || windowPathname === '' ) && navLinkPathname.endsWith('index.html')) || 
+    if ((windowPathname === navLinkPathname) || (isHomePage && navLinkPathname.endsWith('index.html')) || 
         (windowPathname.includes("projects") && navLinkPathname.includes("projects"))) { // This last clause allows the individual project pages to make project the active class so the button can be highlighted
         navLinkEl.classList.add('active');
     }
